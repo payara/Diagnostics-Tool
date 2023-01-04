dd8cd0c958d318198a98e966003170a713072c68
Alan Roth <alan.roth@payara.fish>
2023-01-04 14:57:31 +0000

    init

---
+++ src/main/java/fish/payara/extras/diagnostics/App.java
@@ -0,0 +1,13 @@
+package fish.payara.extras.diagnostics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/fish/payara/extras/AppTest.java
@@ -0,0 +1,20 @@
+package fish.payara.extras;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
