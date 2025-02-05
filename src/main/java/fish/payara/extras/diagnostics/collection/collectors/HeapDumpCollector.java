fd3e8a0eaedabddc415430a7400cfb0eae0a54a3
NotedSalmon <moreiraandre2001@gmail.com>
2025-02-05 12:00:56 +0000

    FISH-10375 spacing for error message

--- src/main/java/fish/payara/extras/diagnostics/collection/collectors/HeapDumpCollector.java
+++ src/main/java/fish/payara/extras/diagnostics/collection/collectors/HeapDumpCollector.java
@@ -105,7 +105,7 @@ public class HeapDumpCollector implements Collector {
             }
 
             if (result.startsWith("Warning:") && result.contains("seems to be offline; command generate-heap-dump was not replicated to that instance")) {
-                LOGGER.warning(target + "is offline! Heap Dump will NOT be collected!");
+                LOGGER.warning(target + " is offline! Heap Dump will NOT be collected!");
             }
         } catch (CommandException e) {
             if (e.getMessage().contains("Remote server does not listen for requests on")) {
